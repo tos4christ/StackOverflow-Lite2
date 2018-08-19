0846f84eaebedcdfdd9a7664969d9596dedb4e34
tos4christ <39365887+tos4christ@users.noreply.github.com>
2018-08-19 21:29:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use((err, req, res,next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('listening on port PORT');
+  console.log(`listening on port ${PORT}`);
 });
 
 export { app };
