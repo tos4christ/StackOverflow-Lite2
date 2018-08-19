eb58e8a00e50a80ce851afbeff1e0be1d8e3737d
tos4christ <39365887+tos4christ@users.noreply.github.com>
2018-08-19 19:56:56 +0100

    Update server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import mocha from 'mocha';
+
 import app from '../app.js';
 
 let question1 = {id: 1, text: 'tosin is doing 48 hours shift duty', answers:[{text: 'this is very true', vote: 2, id: 1},{text: 'are you sure?', id: 2, vote: 3}]};
@@ -9,7 +9,7 @@ let question2 = {id: 2, text: 'tosin is doing 48 hours shift duty', answers:[{te
 let question3 = {id: 3, text: 'tosin is doing 48 hours shift duty', answers:[{text: 'this is very true', vote: 2, id: 1},{text: 'are you sure?', id: 2, vote: 3}]};
 
 let questionArray = [question1,question2,question3];
-
+describe('Start Test', () => {
 it('Test for the get all question Array', (done) => {
   request(app).get('/questions').expect(questionArray).end(done);
 });
@@ -24,4 +24,6 @@ it('Test for the get specific question with id 2', (done) => {
 
 it('Test for the get specific question with id 3', (done) => {
   request(app).get('/questions/3').expect(questionArray[2]).end(done);
+});
+  
 });
